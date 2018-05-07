4d80f127d4ad80506b879d9187757ed8d96931a1
Manuel Iglesias <manuelig@amazon.com>
2018-05-07 16:31:34 -0700

    first commit

---
+++ cli.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+var mqtt = require('mqtt')
+var argv = require('yargs').argv;
+console.log("Connecting...")
+
+var wsh = argv.url;
+var clientId = argv.clientId;
+var topic= argv.topic;
+
+var options = {
+	keepalive: 180,
+	clientId: clientId,
+	protocolId: 'MQTT',
+	protocolVersion: 4,
+	clean: true,
+	reconnectPeriod: 100,
+	connectTimeout: 1000
+};
+
+var client  = mqtt.connect(wsh, options)
+
+client.on('error', function(error) {
+	console.log('error', error)
+})
+
+client.on('packetreceive', function(packet) {
+	console.log("Packet received!", packet)
+})
+
+client.on('packetsend', function(packet) {
+	console.log("Packet sent!", packet)
+})
+
+
+client.on('close', function() {
+	console.log("Closed!")
+	client.end()
+})
+
+client.on('offline', function() {
+	console.log("Offline!")
+})
+
+client.on('connect', function () {
+	console.log("Connected!")
+	try {
+		client.subscribe(topic)
+	} catch (e) {
+		console.log(e)
+	}
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log("Message Received!", message.toString())
+})
+
